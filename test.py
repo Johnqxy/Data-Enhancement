55609808a9583faeecf6b31b513e6ae8c9a26921
boyd <1737003451@qq.com>
2022-07-09 02:37:40 +0800

    multi-class stable

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import cv2
 import json
-str='save/mix2'
+str='save/mix3'
 src=cv2.imread(str+'.jpg')
 with open(str+'.json','r') as f:
     js=json.load(f)
-    for i in js['shapes'][0]['points']:
-        cv2.circle(src,(int(i[0]),int(i[1])),5,(255,0,0),-1)
+    for i in js['shapes']:
+        for j in i['points']:
+            cv2.circle(src,(int(j[0]),int(j[1])),5,(255,0,0),-1)
 
 
 cv2.imshow('res',src)
